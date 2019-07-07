8a2c4decf2de3dfdaa772ff12db955f5d7607ed7
黄彪 <191059014@qq.com>
2019-07-07 18:02:58 +0800

    delete sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-BUILD_ID=gpweb
-cd /data/jenkins/deploy
-cp /var/lib/jenkins/workspace/gp-web ./
-cd gp-web
-nohup cnpm run dev > nohup.out 2>&1 &
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-xl_close_port -p 80
-echo "stop 80 port"
