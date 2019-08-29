06c44f14bf19e6586f5c8f55105559eae109bd23
黄彪 <191059014@qq.com>
2019-08-29 21:43:43 +0800

    更新脚本

--- restart.sh
+++ restart.sh
@@ -7,7 +7,6 @@ then
 fi
 echo "start gp-web..."
 nohup cnpm run dev > nohup.out 2>&1 &
-tail -f nohup.out
 
 # 如果脚本不能正常执行，请参考下面：
 # linux只能执行格式为unix格式的脚本，因为在dos/window下按一次回车键实际上输入的是“回车（CR)”和“换行（LF）”，而Linux/unix下按一次回车键只输入“换行（LF）”，所以修改的sh文件在每行都会多了一个CR，所以Linux下运行时就会报错找不到命令
