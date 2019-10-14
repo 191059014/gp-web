e58946bc9f7ceb03cef52bcd250ea58006deb7b2
Mr.Huang <191059014@qq.com>
2019-10-14 20:12:50 +0800

    修改累计服务费

--- src/views/customerManage/customerFund.vue
+++ src/views/customerManage/customerFund.vue
@@ -25,11 +25,12 @@
       <el-table-column prop="userName" label="客户姓名" style="width:5%"></el-table-column>
       <el-table-column prop="agentName" label="代理商姓名" style="width:6%"></el-table-column>
       <el-table-column prop="accountTotalMoney" label="账户总金额" style="width:20%"></el-table-column>
-      <el-table-column prop="freezeMoney" label="冻结金额" style="width:15%"></el-table-column>
+      <el-table-column prop="freezeMoney" label="提现冻结" style="width:15%"></el-table-column>
+      <el-table-column prop="tradeFreezeMoney" label="交易冻结" style="width:15%"></el-table-column>
       <el-table-column prop="usableMoney" label="可用余额" style="width:6%"></el-table-column>
       <el-table-column prop="totalRechargeMoney" label="累计充值" style="width:6%"></el-table-column>
       <el-table-column prop="totalWithdrawMoney" label="累计提现" style="width:5%"></el-table-column>
-      <el-table-column prop="totalProfitAndLossMoney" label="累计盈亏" style="width:5%"></el-table-column>
+      <el-table-column prop="totalProfitAndLossMoney" label="累计净盈亏" style="width:5%"></el-table-column>
       <el-table-column prop="totalMessageServiceMoney" label="累计信息服务费" style="width:13%"></el-table-column>
       <el-table-column prop="totalInAndOutMoney" label="累计出入金额" style="width:13%"></el-table-column>
       <el-table-column prop="createTime" label="创建时间" style="width:13%"></el-table-column>
